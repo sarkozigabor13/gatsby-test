d9da6c4d9b7af0a6898ac8fa5d7d24974542158e
Sárközi Gábor <sarkozigabor13@gmail.com>
2022-06-14 18:17:18 +0200

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `envimap-test`,
+    description: "An envimap landing page",
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: ["gatsby-plugin-sass"]
