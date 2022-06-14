a235e5560d444577f58e5d0ee02675e9d944a214
Gábor Sárközi <sarkozigabor13@gmail.com>
2022-06-14 21:21:41 +0200

    Test Github Desktop

--- src/pages/index.js
+++ src/pages/index.js
@@ -131,7 +131,7 @@ const IndexPage = () => {
     <main style={pageStyles}>
       <title>Home Page</title>
       <h1 style={headingStyles}>
-        Congratulations - Minden működik.
+        Congratulations - Minden működik. Local repo
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
         <span role="img" aria-label="Party popper emojis">
